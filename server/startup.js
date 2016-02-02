c50e772d936708ac3e5eb542e18c53cbb959ddfe
Mo <mammutit@gmail.com>
2016-02-02 21:34:53 +0100

    added import of products on startup

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,34 @@
- Meteor.startup(function () {
-    // code to run on server at startup
-  });
+ Meteor.startup(function() {
+   var Polet = new Meteor.Collection("Polet");
+
+   if (!Polet.findOne()) {
+     console.log("no products yet... creating from filesystem");
+     var fs = Meteor.npmRequire('fs');
+     var files = fs.readdirSync('./assets/app/');
+     var filename = 'vinmonopolet.json';
+     var poletArray = JSON.parse(Assets.getText(filename));
+
+     for (var k = 0; k < poletArray.length; k++) {
+       var product = poletArray[k];
+       var filteredProduct = {};
+
+       filteredProduct.ProductNumber = product.Varenummer;
+       filteredProduct.ProductName = product.Varenavn;
+       filteredProduct.Volume = product.Volum;
+       filteredProduct.Price = product.Pris;
+       filteredProduct.LiterPrice = product.Literpris;
+       filteredProduct.ProductType = product.Varetype;
+       filteredProduct.Fullness = product.Fylde;
+       filteredProduct.Freshness = product.Friskhet;
+       filteredProduct.Tannin = product.Garvestoffer;
+       filteredProduct.Bitterness = product.Bitterhet;
+       filteredProduct.Sweetness = product.Sodme;
+       filteredProduct.Year = product.Argang;
+       filteredProduct.Alcohol = product.Alkohol;
+       filteredProduct.Shugar = product.Sukker;
+       filteredProduct.Acid = product.Syre;
+
+       Polet.insert(filteredProduct);
+     }
+   }
+ });
