1fe8624a8da14b896ece65b85f2e48cd3d2ad738
Mo <mammutit@gmail.com>
2016-02-03 23:25:28 +0100

    added routes for all main pages

--- client/capstone.js
+++ client/capstone.js
@@ -1,6 +1,17 @@
+Router.configure({
+  layoutTemplate: 'main'
+});
 Router.route('/', function () {
-  this.render('main', {
-  });
+  this.render('landingComponent', {to: 'body'});
+});
+Router.route('/shop', function () {
+  this.render('shopComponent', {to: 'body'});
+});
+Router.route('/checkout', function () {
+  this.render('checkoutComponent', {to: 'body'});
+});
+Router.route('/help', function () {
+  this.render('helpComponent', {to: 'body'});
 });
 
 Template.category.events({
