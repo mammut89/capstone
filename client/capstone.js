45c850664ee273923981e9671eb7870563b941e4
Mo <mammutit@gmail.com>
2016-02-03 23:08:01 +0100

    added more colors, structured templates, began events

--- client/capstone.js
+++ client/capstone.js
@@ -2,3 +2,22 @@ Router.route('/', function () {
   this.render('main', {
   });
 });
+
+Template.category.events({
+    'click .js-selectWhiteWineCategory': function(){
+    },
+    'click .js-selectRedWineCategory': function(){
+    },
+    'click .js-selectRoseWineCategory': function(){
+    },
+    'click .js-selectSparklingWineCategory': function(){
+    },
+    'click .js-selectWhiskyCategory': function(){
+    },
+    'click .js-selectCognacCategory': function(){
+    },
+    'click .js-selectBeerCategory': function(){
+    },
+    'click .js-selectOtherCategory': function(){
+    },
+});
