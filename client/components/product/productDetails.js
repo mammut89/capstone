85bdff8a086e0a3452d2486c1de201a85c0bb84c
Mo <mammutit@gmail.com>
2016-02-06 19:53:09 +0100

    added cart functonality in Session variable and noty animation

--- client/components/product/productDetails.js
+++ client/components/product/productDetails.js
@@ -24,6 +24,30 @@ Template.productDetails.helpers({
     var product = Polet.findOne({
       ProductNumber: productId
     });
+    Session.set("Product", product);
     return product.ProductName;
+  }
+});
+
+Template.productDetails.events({
+  'click .js-add-to-cart': function(a , b , c) {
+    var productId = Number(Iron.Location.get().path.split("/")[2]);
+    var cart = Session.get("Cart");
+    if(!cart) {
+      cart = {};
+    }
+    if(cart[productId]){
+      cart[productId] = cart[productId] + 1;
+    } else {
+      cart[productId] = 1;
+    }
+
+    Session.set("Cart", cart);
+    var product = Session.get("Product");
+    noty({
+      text: 'Added ' + product.ProductName,
+      type: 'success',
+      timeout: 2000
+    });
   }
 });
